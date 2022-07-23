e11d1500ca189ffe2e6bf0177a00bfd08e8f8a4b
JordanChavez21 <60524233+JordanChavez21@users.noreply.github.com>
2022-07-22 19:42:03 -0600

    Resuelto

--- Practicas/Tabla+de+Usuarios/js/ajax.js
+++ Practicas/Tabla+de+Usuarios/js/ajax.js
@@ -5,23 +5,22 @@ document.getElementById('btn_cargar_usuarios').addEventListener('click', functio
    // return false
     var peticion = new XMLHttpRequest();
     //peticion.open('GET', 'https://api.npoint.io/7d3afdf0ba4c085c846b', false);
-    peticion.open('GET', 'php/usuarios.php', false);
+    peticion.open('GET', 'php/prueba.php', false);
 
     loader.classList.add('active');
 
     peticion.onload = function(){
-        //console.log (JSON.parse(peticion.responseText)[0].pais);
-        var datos = JSON.parse(peticion.responseText);
+        // console.log (JSON.parse(peticion.responseText));
+        var datos = JSON.parse(peticion.responseText).Usuarios;     
 
         for (var i = 0; i < datos.length; i++) {
             var elemento = document.createElement('tr');
-            elemento.innerHTML += ("<td>" + datos[i].id + "</td>");
+            elemento.innerHTML += ("<td>" + datos[i].ID + "</td>");
             elemento.innerHTML += ("<td>" + datos[i].nombre + "</td>");
             elemento.innerHTML += ("<td>" + datos[i].edad + "</td>");
             elemento.innerHTML += ("<td>" + datos[i].pais + "</td>");
             elemento.innerHTML += ("<td>" + datos[i].correo + "</td>");
             document.getElementById('tabla').appendChild(elemento);
-            
         }
     }
     /*         datos.forEach(persona => {
@@ -34,8 +33,6 @@ document.getElementById('btn_cargar_usuarios').addEventListener('click', functio
             document.getElementById('tabla').ap[i]pendChild(elemento);
             
         }); */
-
-    
 
     peticion.onreadystatechange = function(){
         console.log(peticion.status)
