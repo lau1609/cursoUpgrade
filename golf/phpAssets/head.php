d0e79dc6351b54beaffe41ba7c7e9ef5e6429684
lau1609 <109751592+lau1609@users.noreply.github.com>
2023-03-17 16:19:29 -0600

    Add files via upload
    Head del proyecto

---
+++ golf/phpAssets/head.php
@@ -0,0 +1,28 @@
+<!-- METAS GENERALES -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1" />
+<meta name="robots" content="follow"/>
+<meta name="author" content=""/>
+<meta name="copyright" content="2023"/>
+
+<!-- FAVICON -->
+<link rel="shortcut icon" type="image/x-icon" href="_images/favicon.ico"/> 
+
+<!-- FONT ASTIC -->
+<!-- <link href="_includes/_fontastic/styles.css" rel="stylesheet" type="text/css"/>  -->
+
+<!-- CSS General -->
+<link rel="stylesheet" type="text/css" href="_includes/_css/large.css">
+<link rel="stylesheet" type="text/css" media="only screen and (max-width:1600px)"  href="_includes/_css/regular.css">
+<link rel="stylesheet" type="text/css" media="only screen and (max-width:1024px)"  href="_includes/_css/medium.css">
+<link rel="stylesheet" type="text/css" media="only screen and (max-width:600px)"   href="_includes/_css/small.css">
+<!-- SPRY -->
+<!--<link rel="stylesheet" type="text/css" href="_includes/_css/SpryValidation.css"/>-->
+ 
+<!--[if lt IE 10]> <script type="text/javascript"> window.location = "./ie/" </script> <![endif]-->
+
+<!-- JS General -->
+<script type="text/javascript" src="_includes/_js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="_includes/_js/main.js"></script>
+<!-- SPRY -->
+<!--<script type="text/javascript" src="_includes/_js/spry/SpryValidationTextField.js"></script>-->
